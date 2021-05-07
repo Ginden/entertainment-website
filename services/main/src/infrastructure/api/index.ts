70f8f729406f93bdaf6593150ac2624a73614931
Michał Wadas <michalwadas@gmail.com>
2021-05-07 18:33:30 +0200

    WIP: Be careful of reading health books, you might die of a misprint. 		-- Mark Twain

--- services/main/src/infrastructure/api/index.ts
+++ services/main/src/infrastructure/api/index.ts
@@ -1,3 +1,6 @@
 import { ServerRoute } from '@hapi/hapi';
+import { imageRoutes } from "./images";
 
-export const routes: ServerRoute[] = [];
+export const routes: ServerRoute[] = [
+  ...imageRoutes,
+];
